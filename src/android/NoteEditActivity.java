cd334beb959479da4a5b7d493a470358ee7674ff
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2024-11-22 17:32:25 +0000

    Update NoteEditActivity.java

--- src/android/NoteEditActivity.java
+++ src/android/NoteEditActivity.java
@@ -14,7 +14,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
-public class NoteEditActivity extends Activity {
+public class NotesEditActivity extends Activity {
 
     private LinearLayout noteLayout;
     private EditText editText;
