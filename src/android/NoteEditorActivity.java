3cb0cd9482fbea7f4283bd8247be98c12ebeb49e
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2024-11-22 14:35:24 +0000

    Update NoteEditorActivity.java

--- src/android/NoteEditorActivity.java
+++ src/android/NoteEditorActivity.java
@@ -623,6 +623,7 @@ import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.view.inputmethod.InputMethodManager;
 
 import androidx.annotation.NonNull;
 
