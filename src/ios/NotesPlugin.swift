aaa54a459960d0dfe1a48611154ec9c8a6d867f7
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2024-11-26 18:50:59 +0000

    Update NotesPlugin.swift

--- src/ios/NotesPlugin.swift
+++ src/ios/NotesPlugin.swift
@@ -1,13 +1,18 @@
-import Foundation
-import Cordova
+import UIKit
 
 @objc(NotesPlugin)
 class NotesPlugin: CDVPlugin {
 
     @objc(openNotesList:)
     func openNotesList(command: CDVInvokedUrlCommand) {
-        // Create an instance of the NotesListViewController
-        let notesListVC = NotesListViewController()
+        // Try to instantiate the NotesListViewController from the main storyboard
+        guard let storyboard = UIStoryboard(name: "Main", bundle: nil) as UIStoryboard?,
+              let notesListVC = storyboard.instantiateViewController(withIdentifier: "NotesListViewController") as? UIViewController else {
+            // Return an error if the view controller cannot be instantiated
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Failed to open notes list")
+            self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+            return
+        }
 
         // Present the NotesListViewController
         self.viewController?.present(notesListVC, animated: true, completion: nil)
