87ddbb9232dc21ab8c38ff5b26ec14fb459fc61c
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2024-11-26 18:51:35 +0000

    Update NotesPlugin.m

--- src/ios/NotesPlugin.m
+++ src/ios/NotesPlugin.m
@@ -1,24 +1,18 @@
-import UIKit
+#import "NotesPlugin.h"
+#import "NotesListViewController.h" // Assumes a view controller for the notes list exists
 
-@objc(NotesPlugin)
-class NotesPlugin: CDVPlugin {
+@implementation NotesPlugin
 
-    @objc(openNotesList:)
-    func openNotesList(command: CDVInvokedUrlCommand) {
-        // Try to instantiate the NotesListViewController from the main storyboard
-        guard let storyboard = UIStoryboard(name: "Main", bundle: nil) as UIStoryboard?,
-              let notesListVC = storyboard.instantiateViewController(withIdentifier: "NotesListViewController") as? UIViewController else {
-            // Return an error if the view controller cannot be instantiated
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Failed to open notes list")
-            self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-            return
-        }
-
-        // Present the NotesListViewController
-        self.viewController?.present(notesListVC, animated: true, completion: nil)
-
-        // Send success callback to JavaScript
-        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
-        self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-    }
+- (void)openNotesList:(CDVInvokedUrlCommand *)command {
+    // Create an instance of the NotesListViewController
+    NotesListViewController *notesListVC = [[NotesListViewController alloc] init];
+    
+    // Present the NotesListViewController
+    [self.viewController presentViewController:notesListVC animated:YES completion:nil];
+    
+    // Send success callback to JavaScript
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
+
+@end
