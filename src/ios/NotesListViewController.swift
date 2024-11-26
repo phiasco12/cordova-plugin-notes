048674b7647bc42d1e3269cf51fc2053900bc469
phiasco12 <50844518+phiasco12@users.noreply.github.com>
2024-11-26 18:58:29 +0000

    Update NotesListViewController.swift

--- src/ios/NotesListViewController.swift
+++ src/ios/NotesListViewController.swift
@@ -2,14 +2,13 @@ import UIKit
 
 class NotesListViewController: UIViewController {
 
-    // Properties
-    var notesScrollView: UIScrollView!
-    var savedNotes: [String] = []
-    var notesDirectory: String!
+    private var notesScrollView: UIScrollView!
+    private var savedNotes: [String] = []
+    private var notesDirectory: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Set up the view
         view.backgroundColor = .white
         notesDirectory = getNotesDirectory()
@@ -34,40 +33,39 @@ class NotesListViewController: UIViewController {
     }
 
     // Get the path to the notes directory
-    func getNotesDirectory() -> String {
+    private func getNotesDirectory() -> String {
         let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
         let documentsDirectory = paths.first!
-        let notesDirectory = (documentsDirectory as NSString).appendingPathComponent("saved_notes")
-
-        let fileManager = FileManager.default
-        if !fileManager.fileExists(atPath: notesDirectory) {
-            try? fileManager.createDirectory(atPath: notesDirectory, withIntermediateDirectories: true, attributes: nil)
+        let notesDirectory = documentsDirectory.appending("/saved_notes")
+        
+        if !FileManager.default.fileExists(atPath: notesDirectory) {
+            try? FileManager.default.createDirectory(atPath: notesDirectory, withIntermediateDirectories: true, attributes: nil)
         }
         return notesDirectory
     }
 
     // Load all saved notes and display them
-    func loadSavedNotes() {
+    private func loadSavedNotes() {
         savedNotes.removeAll()
         notesScrollView.subviews.forEach { $0.removeFromSuperview() }
 
         let fileManager = FileManager.default
-        let files = (try? fileManager.contentsOfDirectory(atPath: notesDirectory)) ?? []
-
+        guard let files = try? fileManager.contentsOfDirectory(atPath: notesDirectory) else { return }
+        
         var x: CGFloat = 20
         var y: CGFloat = 20
         let noteSize: CGFloat = 100
 
         for fileName in files {
             if fileName.hasSuffix(".png") {
-                let notePath = (notesDirectory as NSString).appendingPathComponent(fileName)
-                let jsonPath = (notePath as NSString).deletingPathExtension + ".json"
-
+                let notePath = notesDirectory.appending("/\(fileName)")
+                let jsonPath = (notePath as NSString).deletingPathExtension.appending(".json")
+                
                 if fileManager.fileExists(atPath: jsonPath) {
                     savedNotes.append(fileName)
                     addNoteToScrollView(noteFileName: fileName, at: CGPoint(x: x, y: y))
                     x += noteSize + 20
-
+                    
                     if x + noteSize > view.frame.size.width {
                         x = 20
                         y += noteSize + 20
@@ -79,35 +77,35 @@ class NotesListViewController: UIViewController {
     }
 
     // Add a note to the scroll view
-    func addNoteToScrollView(noteFileName: String, at position: CGPoint) {
-        let notePath = (notesDirectory as NSString).appendingPathComponent(noteFileName)
-        if let noteImage = UIImage(contentsOfFile: notePath) {
-            let noteButton = UIButton(type: .custom)
-            noteButton.frame = CGRect(x: position.x, y: position.y, width: 100, height: 100)
-            noteButton.setImage(noteImage, for: .normal)
-            noteButton.layer.cornerRadius = 10
-            noteButton.clipsToBounds = true
-            noteButton.backgroundColor = .lightGray
-            noteButton.tag = savedNotes.firstIndex(of: noteFileName) ?? 0
-            noteButton.addTarget(self, action: #selector(openNoteEditor(_:)), for: .touchUpInside)
-            notesScrollView.addSubview(noteButton)
-        }
+    private func addNoteToScrollView(noteFileName: String, at position: CGPoint) {
+        let notePath = notesDirectory.appending("/\(noteFileName)")
+        guard let noteImage = UIImage(contentsOfFile: notePath) else { return }
+
+        let noteButton = UIButton(type: .custom)
+        noteButton.frame = CGRect(x: position.x, y: position.y, width: 100, height: 100)
+        noteButton.setImage(noteImage, for: .normal)
+        noteButton.layer.cornerRadius = 10
+        noteButton.clipsToBounds = true
+        noteButton.backgroundColor = .lightGray
+        noteButton.tag = savedNotes.firstIndex(of: noteFileName) ?? 0
+        noteButton.addTarget(self, action: #selector(openNoteEditor(_:)), for: .touchUpInside)
+        notesScrollView.addSubview(noteButton)
     }
 
     // Open the note editor for a specific note
-    @objc func openNoteEditor(_ sender: UIButton) {
+    @objc private func openNoteEditor(_ sender: UIButton) {
         let noteFileName = savedNotes[sender.tag]
         let noteEditor = NoteEditorViewController()
-        noteEditor.noteFileName = noteFileName // Assuming noteFileName is a property of NoteEditorViewController
+        noteEditor.noteFileName = noteFileName
         present(noteEditor, animated: true, completion: nil)
     }
 
     // Open the note creator
-    @objc func openNoteCreator() {
+    @objc private func openNoteCreator() {
         let noteEditor = NoteEditorViewController()
-        present(noteEditor, animated: true) {
+        present(noteEditor, animated: true) { [weak self] in
             // Optionally reload after note creation
-            self.loadSavedNotes()
+            self?.loadSavedNotes()
         }
     }
 }
